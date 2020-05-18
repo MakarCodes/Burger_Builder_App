e9ff6681da210883ef68cd72839b37b5db5e635f
MakarCodes <jarek.strupek@wp.pl>
2020-05-18 11:05:35 +0200

    fixed bug during reload of the page - ingredients were null

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 import ContactData from './ContactData/ContactData'
@@ -8,23 +8,30 @@ import ContactData from './ContactData/ContactData'
 const Checkout = (props) => {
     const checkoutCancelHandler = () => props.history.goBack();
     const checkoutContinueHandler = () => props.history.replace('/checkout/contact-data');
-        return (
+    let checkoutSummary = <Redirect to='/'/>;
+    if(props.ingredients) {
+        const purchaseRedirect = props.purchased ? <Redirect to='/'/> : null
+        checkoutSummary = (
             <div>
+                { purchaseRedirect }
                 <CheckoutSummary
-                    checkoutCancelled={checkoutCancelHandler}
-                    checkoutContinued={checkoutContinueHandler}
-                    ingredients={props.ingredients}/>
+                checkoutCancelled={checkoutCancelHandler}
+                checkoutContinued={checkoutContinueHandler}
+                ingredients={props.ingredients}/>
                 <Route 
                     path={props.match.path + '/contact-data'} 
                     component={ContactData}/>
             </div>
-        );
+        )
+    }
+        return checkoutSummary;
 }
 
 
 const mapStateToProps = state => {
     return {
-        ingredients: state.burgerBuilder.ingredients
+        ingredients: state.burgerBuilder.ingredients,
+        purchased: state.orders.purchased
     }
 }
 
