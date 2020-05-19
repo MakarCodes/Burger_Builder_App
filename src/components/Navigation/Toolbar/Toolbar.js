cda8191ffb0cf27ac5117cc53a24279365fc6a0c
MakarCodes <jarek.strupek@wp.pl>
2020-05-19 16:51:58 +0200

    fixed problem with redirections while being logged or not and ordering

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,7 +4,6 @@ import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems'
 import SideDrawerToggle from '../SideDrawer/SideDrawerToggle/SideDrawerToggle'
-import { checkPropTypes } from 'prop-types';
 
 const Toolbar = ({sideDrawerClicked, isAuth}) => {
     return (
