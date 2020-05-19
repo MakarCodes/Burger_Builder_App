09f55ce518265cae3bbc7dca216224031b509640
MakarCodes <jarek.strupek@wp.pl>
2020-05-19 12:06:40 +0200

    added auth component

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+
+import classes from './Auth.module.css'
+import Input from '../../components/UI/FormInput/FormInput'
+import Button from '../../components/UI/Button/Button'
+
+class Auth extends Component {
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email Adress'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    }
+
+    checkValidity = (value, rules) => {
+        let isValid = true;
+   
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+        if(rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid; 
+        }
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+        return isValid;
+    }
+
+    inputChangeHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        };
+        this.setState({controls: updatedControls});
+    }
+
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key]
+            });
+        }
+
+        const form = formElementsArray.map(inputElement => {
+            return <Input 
+                        key={inputElement.id} 
+                        elementType={inputElement.config.elementType} 
+                        elementConfig= {inputElement.config.elementConfig}
+                        invalid={!inputElement.config.valid}
+                        touched={inputElement.config.touched}
+                        validationIsRequired={inputElement.config.validation}
+                        value= {inputElement.config.value}
+                        change={(event) => {this.inputChangeHandler(event, inputElement.id)}}/> 
+        })
+        return (
+            <div className={classes.Auth}>
+                <form>
+                    {form}
+                    <Button btnType='Success'>SUBMIT</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Auth;
