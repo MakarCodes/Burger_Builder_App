240268490444750ddc2f7c597ea339a4bb47b6a1
MakarCodes <jarek.strupek@wp.pl>
2020-05-19 21:03:23 +0200

    Updates

---
+++ precache-manifest.2b9a5249fc5e0a88469d8f617b09452e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50886d019ac36c10240c76df44efa3ac",
+    "url": "/Burger_Builder_App/index.html"
+  },
+  {
+    "revision": "fbe6ab23a74907edb3a1",
+    "url": "/Burger_Builder_App/static/css/main.c986c7c2.chunk.css"
+  },
+  {
+    "revision": "6b97216273f401c7cf99",
+    "url": "/Burger_Builder_App/static/js/2.e1fbc624.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Burger_Builder_App/static/js/2.e1fbc624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe6ab23a74907edb3a1",
+    "url": "/Burger_Builder_App/static/js/main.9967a5fd.chunk.js"
+  },
+  {
+    "revision": "90bdc8f5ce0b30937329",
+    "url": "/Burger_Builder_App/static/js/runtime-main.a50862a6.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger_Builder_App/static/media/burger-logo.b8503d26.png"
+  }
+]);
